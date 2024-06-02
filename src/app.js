785ca600e537fe2968a38cd8ad087df028729a4e
AlessandroWindson <AlessandroWindsonMM@gmail.com>
2024-06-02 17:05:32 -0400

    "Web - Back-End

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 import express from 'express'
-import SelecaoController from './app/controllers/SelecaoController.js'
+import routes from './routes.js'
 
 const app = express()
 
 //indicar para o express ler body com json
 app.use(express.json())
+// usar o router
 
-//Rotas
-app.get('/selecoes', SelecaoController.index)
-app.get('/selecoes/:id', SelecaoController.show)
-app.post('/selecoes', SelecaoController.store)
-app.put('/selecoes/:id', SelecaoController.update)
-app.delete('/selecoes/:id', SelecaoController.delete)
+app.use(routes)
 
 export default app
